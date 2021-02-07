58845d8d688f1c3abcc5b7d4319e9426a5c09096
Eduardo Lagunas <elagunas@aenor.com>
2021-02-07 17:38:35 +0100

    my first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mycustomproject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
